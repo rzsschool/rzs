a30fe3e843de72d97a60861085b213a742f80a5d
rzs <site.administrator@rzsgymn.ukr.education>
2022-09-04 23:54:08 +0300

    menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,55 @@
     "use strict";
     
     // Dropdown on mouse hover
+    // $(document).ready(function () {
+    //     function toggleNavbarMethod() {
+    //         if ($(window).width() > 992) {
+    //             $('.navbar .dropdown').on('mouseover', function () {
+    //                 $('.dropdown-toggle', this).trigger('click');
+    //             }).on('mouseout', function () {
+    //                 $('.dropdown-toggle', this).trigger('click').blur();
+    //             });
+    //         } else {
+    //             $('.navbar .dropdown').off('mouseover').off('mouseout');
+    //         }
+    //     }
+    //     toggleNavbarMethod();
+    //     $(window).resize(toggleNavbarMethod);
+    // });
+    $(document).ready(function () {
+        let menuItem = document.getElementsByClassName('menu-item-has-children');
+        
+        const notShow = (i=-1) => {
+            for (let index = 0; index < menuItem.length; index++) {
+                let element = menuItem[index].lastElementChild;
+                if (i == index) {
+                    continue;
+                }
+                if (element.classList.contains('show')){
+                    element.classList.remove('show');
+                }
+            }
+        }
+
+        for (let index = 0; index < menuItem.length; index++) {
+            
+            let element = menuItem[index];
+     
+            element.firstElementChild.classList.add('dropdown-toggle');
+
+            element.addEventListener('click', ()=>{
+                if (window.innerWidth <= 991) {
+                    let subMenu = element.getElementsByClassName('sub-menu');
+                    notShow(index);
+                    subMenu[0].classList.toggle('show');                
+                } else {
+                    notShow();
+                }
+            });
+        }
+
+    });
+
     $(document).ready(function () {
         function toggleNavbarMethod() {
             if ($(window).width() > 992) {
